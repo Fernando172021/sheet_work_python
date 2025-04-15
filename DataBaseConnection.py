6b288fe824de39a7f0fd24aa3e988b0d0b4196af
Fernando_DEV <fernandoperuci2004@gmail.com>
2025-04-14 22:11:49 -0300

    Codificanção da logica de INSERT, DELETE e SELECT para com o Banco de Dados

--- DataBaseConnection.py
+++ DataBaseConnection.py
@@ -3,7 +3,7 @@ from instances import Pessoa_Dados
 import pymysql
 
 class DataBaseInsert:
-    def __init__(self):
+    def __init__(self):   
         con = pymysql.connect(
             host= db_config['host'],
             user= db_config['user'],
@@ -26,14 +26,16 @@ class DataBaseInsert:
             self.cursor.close()
             con.close()
             print('Conexão encerrada!')
+                
 
     def insert_data_user(self):
         personData = Pessoa_Dados
-        dataSingUp = Pessoa_Dados.data_for_signup
-
+        dataSingUp = personData.data_for_signup
+        
         insert_command = f'INSERT INTO {db_config['table_user']} () VALUES ("{dataSingUp[personData.id_cadastro_nome][0]}","{dataSingUp[personData.id_cadastro_sobrenome][0]}","{dataSingUp[personData.id_cadastro_data_nascimento][0]}","{dataSingUp[personData.id_cadastro_numero_telefone][0]}","{dataSingUp[personData.id_cadastro_email][0]}","{dataSingUp[personData.id_cadastro_logradouro][0]}","{dataSingUp[personData.id_cadastro_numero_casa][0]}","{dataSingUp[personData.id_cadastro_complemento][0]}","{dataSingUp[personData.id_cadastro_bairro][0]}","{dataSingUp[personData.id_cadastro_cidade][0]}","{dataSingUp[personData.id_cadastro_cep][0]}","{dataSingUp[personData.id_cadastro_estado][0]}");' 
 
         return insert_command
+
 
 class DataBaseDelete:
     def __init__(self):
@@ -65,8 +67,41 @@ class DataBaseDelete:
     
     def delete_data_user(self):
         personData = Pessoa_Dados
-        dataDelete = Pessoa_Dados.data_for_signup
+        dataDelete = personData.data_for_signup
 
         self.delete_command = f'DELETE FROM {db_config['table_user']} WHERE EMAIL = "{dataDelete[personData.id_cadastro_email][0]}";'
 
         return self.delete_command
+    
+class DataBaseSelect:
+    def __init__(self):
+        con = pymysql.connect(
+            host= db_config['host'],
+            user= db_config['user'],
+            password= db_config['password'],
+            database= db_config['database'],
+        )
+
+        try:
+            self.cursor = con.cursor()
+            self.cursor.execute(self.select_data_user())
+            con.commit()
+            
+            print(self.cursor.fetchall())
+
+        except pymysql.Error as a:
+            print(f'Erro ao inserir dados: {a}')
+            con.rollback()
+
+        finally:
+            self.cursor.close()
+            con.close()
+            print('Conexão encerrada!')
+
+    def select_data_user(self):
+        personData = Pessoa_Dados
+        dataSelect = personData.data_for_signup
+
+        self.select_command = f'SELECT * FROM {db_config['table_user']} WHERE EMAIL = "{dataSelect[personData.id_cadastro_email][0]}";'
+
+        return self.select_command
